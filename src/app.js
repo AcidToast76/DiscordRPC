5ae795b8c9b4c0effadf1b38a9899f478e49e3d5
AcidToast76 <letukasjosh77@gmail.com>
2024-06-22 22:10:38 +0100

    ADded extra code

--- src/app.js
+++ src/app.js
@@ -1,2 +1,70 @@
 const clientId = "1254145695762485248";
 const DiscordRPC = require("discord-rpc");
+const RPC = new DiscordRPC.Client({ transport: "ipc" });
+
+DiscordRPC.register(clientId);
+
+async function spotifyrpc (url) {
+   let spotifyRPC = await fetch("https://spotify.thefemdevs.com/playing/nezha").then(res => res.json())
+   console.log(spotifyRPC)
+}
+
+spotifyrpc()
+
+async function setActivity() {
+    if (!RPC) {
+        return;
+    }
+    
+    RPC.setActivity({
+        details: "Playing a game",
+        state: "In the menu",
+        largeImageKey: "large",
+        largeImageText: "Large Image",
+        smallImageKey: "small",
+        smallImageText: "Small Image",
+        instance: false,
+        buttons: [
+            { 
+                label: "Website", url: "https://nezha.thefemdevs.com" 
+            },
+            { 
+                label: "Spotify", url: "https://open.spotify.com/user/hpjv09y99ucx7adf3jb6qijvp?si=40ac1fa71e7d44ee" 
+            }
+        ]
+    });
+}
+
+async function setSpotifyRPC() {
+    if (!RPC) {
+        return;
+    }
+    
+    RPC.setActivity({
+        details: "Listening to Spotify",
+        state: "Warframe Bops",
+        largeImageKey: "large",
+        largeImageText: "Large Image",
+        smallImageKey: "small",
+        smallImageText: "Small Image",
+        instance: false,
+        buttons: [
+            { 
+                label: "Website", url: "https://nezha.thefemdevs.com" 
+            },
+            { 
+                label: "Spotify", url: "https://open.spotify.com/user/hpjv09y99ucx7adf3jb6qijvp?si=40ac1fa71e7d44ee" 
+            }
+        ]
+    });
+
+}
+
+RPC.on("ready", async () => {
+    setActivity();
+    setInterval(() => {
+        setActivity();
+    }, 15e3);
+});
+
+RPC.login({ clientId }).catch(err => console.error(err));
--- src/dist/app.dev.js
+++ src/dist/app.dev.js
@@ -3,4 +3,133 @@
 var clientId = "1254145695762485248";
 
 var DiscordRPC = require("discord-rpc");
+
+var RPC = new DiscordRPC.Client({
+  transport: "ipc"
+});
+DiscordRPC.register(clientId);
+
+function spotifyrpc(url) {
+  var spotifyRPC;
+  return regeneratorRuntime.async(function spotifyrpc$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return regeneratorRuntime.awrap(fetch("https://spotify.thefemdevs.com/playing/nezha").then(function (res) {
+            return res.json();
+          }));
+
+        case 2:
+          spotifyRPC = _context.sent;
+          console.log(spotifyRPC);
+
+        case 4:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+}
+
+spotifyrpc();
+
+function setActivity() {
+  return regeneratorRuntime.async(function setActivity$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          if (RPC) {
+            _context2.next = 2;
+            break;
+          }
+
+          return _context2.abrupt("return");
+
+        case 2:
+          RPC.setActivity({
+            details: "Playing a game",
+            state: "In the menu",
+            largeImageKey: "large",
+            largeImageText: "Large Image",
+            smallImageKey: "small",
+            smallImageText: "Small Image",
+            instance: false,
+            buttons: [{
+              label: "Website",
+              url: "https://nezha.thefemdevs.com"
+            }, {
+              label: "Spotify",
+              url: "https://open.spotify.com/user/hpjv09y99ucx7adf3jb6qijvp?si=40ac1fa71e7d44ee"
+            }]
+          });
+
+        case 3:
+        case "end":
+          return _context2.stop();
+      }
+    }
+  });
+}
+
+function setSpotifyRPC() {
+  return regeneratorRuntime.async(function setSpotifyRPC$(_context3) {
+    while (1) {
+      switch (_context3.prev = _context3.next) {
+        case 0:
+          if (RPC) {
+            _context3.next = 2;
+            break;
+          }
+
+          return _context3.abrupt("return");
+
+        case 2:
+          RPC.setActivity({
+            details: "Listening to Spotify",
+            state: "Warframe Bops",
+            largeImageKey: "large",
+            largeImageText: "Large Image",
+            smallImageKey: "small",
+            smallImageText: "Small Image",
+            instance: false,
+            buttons: [{
+              label: "Website",
+              url: "https://nezha.thefemdevs.com"
+            }, {
+              label: "Spotify",
+              url: "https://open.spotify.com/user/hpjv09y99ucx7adf3jb6qijvp?si=40ac1fa71e7d44ee"
+            }]
+          });
+
+        case 3:
+        case "end":
+          return _context3.stop();
+      }
+    }
+  });
+}
+
+RPC.on("ready", function _callee() {
+  return regeneratorRuntime.async(function _callee$(_context4) {
+    while (1) {
+      switch (_context4.prev = _context4.next) {
+        case 0:
+          setActivity();
+          setInterval(function () {
+            setActivity();
+          }, 15e3);
+
+        case 2:
+        case "end":
+          return _context4.stop();
+      }
+    }
+  });
+});
+RPC.login({
+  clientId: clientId
+})["catch"](function (err) {
+  return console.error(err);
+});
 //# sourceMappingURL=app.dev.js.map
