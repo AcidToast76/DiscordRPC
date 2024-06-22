81137f3bfcd062b923be28e9949b30ab62ec4859
AcidToast76 <letukasjosh77@gmail.com>
2024-06-22 20:10:02 +0100

    adding discord rpc api

---
+++ src/app.js
@@ -0,0 +1,2 @@
+const clientId = "1254145695762485248";
+const DiscordRPC = require("discord-rpc");
---
+++ src/dist/app.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var clientId = "1254145695762485248";
+
+var DiscordRPC = require("discord-rpc");
+//# sourceMappingURL=app.dev.js.map
