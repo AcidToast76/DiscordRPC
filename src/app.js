9df88499881aa98c64ea11450428ffa58528817f
AcidToast76 <letukasjosh77@gmail.com>
2024-06-24 07:31:31 +0100

    finally working! album cover next

--- src/app.js
+++ src/app.js
@@ -2,86 +2,43 @@ const clientId = "1254145695762485248";
 const DiscordRPC = require("discord-rpc");
 const RPC = new DiscordRPC.Client({ transport: "ipc" });
 
-
-DiscordRPC.register(clientId);
-
-async function setActivity() {
-    let spotifyRPC = await fetch("https://spotify.thefemdevs.com/playing/nezha").then(res => res.json());
-    console.dir(spotifyRPC);
-    if (!RPC) {
-        return;
-    }
-    if (!spotifyRPC) {
-        return RPC.clearActivity();
-    }
-    const { name: title, artists, album, url } = spotifyRPC.song
-    
-    RPC.setActivity({
-        state: "Listening To:",
-        details: title, " by ": artists, " on ": album,
-        largeImageKey: "large",
-        largeImageText: "Large Image",
-        smallImageKey: "small",
-        smallImageText: "Small Image",
-        instance: false,
-        buttons: [
-            { 
-                label: "Website", url: "https://nezha.thefemdevs.com" 
-            },
-            { 
-                label: "Spotify", url: "https://open.spotify.com/user/hpjv09y99ucx7adf3jb6qijvp?si=40ac1fa71e7d44ee" 
-            },
-        ]
-    });
+async function spotifyJSONfetch(url) {
+    let spotifyData = await fetch('https://spotify.thefemdevs.com/playing/nezha').then(res => res.json())
+    console.log(spotifyData)
+    return spotifyData;
 }
 
-RPC.on("ready", async () => {
-    setActivity();
-    setInterval(() => {
-        setActivity();
-    }, 15e3);
-});
-
-
-// Function to fetch the currently playing track from Spotify
-async function fetchCurrentlyPlayingTrack() {
-    const response = await fetch("https://spotify.thefemdevs.com/playing/nezha", {
-    });
-
-    if (!response.ok) {
-        throw new Error("Failed to fetch currently playing track");
-    }
-
-    const data = await response.json();
-    return {
-        trackName: data.item.name,
-        artistName: data.item.artists.map(artist => artist.name).join(", "),
-        trackUrl: data.item.external_urls.spotify,
-    };
-}
+spotifyJSONfetch();
 
 // Function to update Discord RPC with Spotify track information
 async function updateDiscordRPCWithSpotify() {
-    try {
-        const { trackName, artistName, trackUrl } = await fetchCurrentlyPlayingTrack('https://spotify.thefemdevs.com/playing/nezha');
-
-        RPC.setActivity({
-            details: `Listening to ${trackName}`,
-            state: `by ${artistName}`,
-            largeImageKey: "spotify",
-            buttons: [{ label: "Listen on Spotify", url: trackUrl }]
-        });
-    } catch (error) {
-        console.error("Failed to update Discord RPC with Spotify:", error);
+    let spotifyData = await fetch('https://spotify.thefemdevs.com/playing/nezha').then(res => res.json());
+    console.dir(spotifyData);
+    if(!RPC) {
+        return;
     }
+
+    const { name, artists, album, url} = spotifyData.song
+    
+    RPC.setActivity({
+        details: "Listening To Spotify",
+        state: `${name} by ${artists} on ${album}`,
+        largeImageKey: "spotify",
+        largeImageText: album,
+        buttons: [
+            { label: "Listen on Spotify", url },
+            { label: "Website", url: "https://nezha.thefemdevs.com"},
+        ],
+    })
+
+
 }
 
-// Example usage
 RPC.on("ready", async () => {
-    setActivity();
-    setInterval(setActivity, 15e3); // Update every 15 seconds
+    updateDiscordRPCWithSpotify();
+    setInterval(() => {
+        updateDiscordRPCWithSpotify();
+    }, 15e3);
 });
 
 RPC.login({ clientId }).catch(err => console.error(err));
-
-setActivity();
--- src/dist/app.dev.js
+++ src/dist/app.dev.js
@@ -7,61 +7,24 @@ var DiscordRPC = require("discord-rpc");
 var RPC = new DiscordRPC.Client({
   transport: "ipc"
 });
-DiscordRPC.register(clientId);
 
-function setActivity() {
-  var spotifyRPC, _spotifyRPC$song, title, artists, album, url;
-
-  return regeneratorRuntime.async(function setActivity$(_context) {
+function spotifyJSONfetch(url) {
+  var spotifyData;
+  return regeneratorRuntime.async(function spotifyJSONfetch$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
           _context.next = 2;
-          return regeneratorRuntime.awrap(fetch("https://spotify.thefemdevs.com/playing/nezha").then(function (res) {
+          return regeneratorRuntime.awrap(fetch('https://spotify.thefemdevs.com/playing/nezha').then(function (res) {
             return res.json();
           }));
 
         case 2:
-          spotifyRPC = _context.sent;
-          console.dir(spotifyRPC);
+          spotifyData = _context.sent;
+          console.log(spotifyData);
+          return _context.abrupt("return", spotifyData);
 
-          if (RPC) {
-            _context.next = 6;
-            break;
-          }
-
-          return _context.abrupt("return");
-
-        case 6:
-          if (spotifyRPC) {
-            _context.next = 8;
-            break;
-          }
-
-          return _context.abrupt("return", RPC.clearActivity());
-
-        case 8:
-          _spotifyRPC$song = spotifyRPC.song, title = _spotifyRPC$song.name, artists = _spotifyRPC$song.artists, album = _spotifyRPC$song.album, url = _spotifyRPC$song.url;
-          RPC.setActivity({
-            state: "Listening To:",
-            details: title,
-            " by ": artists,
-            " on ": album,
-            largeImageKey: "large",
-            largeImageText: "Large Image",
-            smallImageKey: "small",
-            smallImageText: "Small Image",
-            instance: false,
-            buttons: [{
-              label: "Website",
-              url: "https://nezha.thefemdevs.com"
-            }, {
-              label: "Spotify",
-              url: "https://open.spotify.com/user/hpjv09y99ucx7adf3jb6qijvp?si=40ac1fa71e7d44ee"
-            }]
-          });
-
-        case 10:
+        case 5:
         case "end":
           return _context.stop();
       }
@@ -69,119 +32,68 @@ function setActivity() {
   });
 }
 
-RPC.on("ready", function _callee() {
-  return regeneratorRuntime.async(function _callee$(_context2) {
+spotifyJSONfetch(); // Function to update Discord RPC with Spotify track information
+
+function updateDiscordRPCWithSpotify() {
+  var spotifyData, _spotifyData$song, name, artists, album, url;
+
+  return regeneratorRuntime.async(function updateDiscordRPCWithSpotify$(_context2) {
     while (1) {
       switch (_context2.prev = _context2.next) {
         case 0:
-          setActivity();
-          setInterval(function () {
-            setActivity();
-          }, 15e3);
+          _context2.next = 2;
+          return regeneratorRuntime.awrap(fetch('https://spotify.thefemdevs.com/playing/nezha').then(function (res) {
+            return res.json();
+          }));
 
         case 2:
+          spotifyData = _context2.sent;
+          console.dir(spotifyData);
+
+          if (RPC) {
+            _context2.next = 6;
+            break;
+          }
+
+          return _context2.abrupt("return");
+
+        case 6:
+          _spotifyData$song = spotifyData.song, name = _spotifyData$song.name, artists = _spotifyData$song.artists, album = _spotifyData$song.album, url = _spotifyData$song.url;
+          RPC.setActivity({
+            details: "Listening To Spotify",
+            state: "".concat(name, " by ").concat(artists, " on ").concat(album),
+            largeImageKey: "spotify",
+            largeImageText: album,
+            buttons: [{
+              label: "Listen on Spotify",
+              url: url
+            }, {
+              label: "Website",
+              url: "https://nezha.thefemdevs.com"
+            }]
+          });
+
+        case 8:
         case "end":
           return _context2.stop();
       }
     }
   });
-}); // Function to fetch the currently playing track from Spotify
+}
 
-function fetchCurrentlyPlayingTrack() {
-  var response, data;
-  return regeneratorRuntime.async(function fetchCurrentlyPlayingTrack$(_context3) {
+RPC.on("ready", function _callee() {
+  return regeneratorRuntime.async(function _callee$(_context3) {
     while (1) {
       switch (_context3.prev = _context3.next) {
         case 0:
-          _context3.next = 2;
-          return regeneratorRuntime.awrap(fetch("https://spotify.thefemdevs.com/playing/nezha", {}));
+          updateDiscordRPCWithSpotify();
+          setInterval(function () {
+            updateDiscordRPCWithSpotify();
+          }, 15e3);
 
         case 2:
-          response = _context3.sent;
-
-          if (response.ok) {
-            _context3.next = 5;
-            break;
-          }
-
-          throw new Error("Failed to fetch currently playing track");
-
-        case 5:
-          _context3.next = 7;
-          return regeneratorRuntime.awrap(response.json());
-
-        case 7:
-          data = _context3.sent;
-          return _context3.abrupt("return", {
-            trackName: data.item.name,
-            artistName: data.item.artists.map(function (artist) {
-              return artist.name;
-            }).join(", "),
-            trackUrl: data.item.external_urls.spotify
-          });
-
-        case 9:
         case "end":
           return _context3.stop();
-      }
-    }
-  });
-} // Function to update Discord RPC with Spotify track information
-
-
-function updateDiscordRPCWithSpotify() {
-  var _ref, trackName, artistName, trackUrl;
-
-  return regeneratorRuntime.async(function updateDiscordRPCWithSpotify$(_context4) {
-    while (1) {
-      switch (_context4.prev = _context4.next) {
-        case 0:
-          _context4.prev = 0;
-          _context4.next = 3;
-          return regeneratorRuntime.awrap(fetchCurrentlyPlayingTrack('https://spotify.thefemdevs.com/playing/nezha'));
-
-        case 3:
-          _ref = _context4.sent;
-          trackName = _ref.trackName;
-          artistName = _ref.artistName;
-          trackUrl = _ref.trackUrl;
-          RPC.setActivity({
-            details: "Listening to ".concat(trackName),
-            state: "by ".concat(artistName),
-            largeImageKey: "spotify",
-            buttons: [{
-              label: "Listen on Spotify",
-              url: trackUrl
-            }]
-          });
-          _context4.next = 13;
-          break;
-
-        case 10:
-          _context4.prev = 10;
-          _context4.t0 = _context4["catch"](0);
-          console.error("Failed to update Discord RPC with Spotify:", _context4.t0);
-
-        case 13:
-        case "end":
-          return _context4.stop();
-      }
-    }
-  }, null, null, [[0, 10]]);
-} // Example usage
-
-
-RPC.on("ready", function _callee2() {
-  return regeneratorRuntime.async(function _callee2$(_context5) {
-    while (1) {
-      switch (_context5.prev = _context5.next) {
-        case 0:
-          setActivity();
-          setInterval(setActivity, 15e3); // Update every 15 seconds
-
-        case 2:
-        case "end":
-          return _context5.stop();
       }
     }
   });
@@ -191,5 +103,4 @@ RPC.login({
 })["catch"](function (err) {
   return console.error(err);
 });
-setActivity();
 //# sourceMappingURL=app.dev.js.map
